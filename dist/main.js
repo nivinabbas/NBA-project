2d54456ebf1b1b7264229ee2e58ab6d04cee3ada
nivinabbas <nivin.abbas1@gmail.com>
2021-12-08 01:19:16 +0200

    add two on click functions

--- dist/main.js
+++ dist/main.js
@@ -12,3 +12,36 @@ $('.getTeamName').on('click', function () {
     $('.teamCard').append(newHTML);
   });
 });
+
+$('#dreamTeam').on('click', function () {
+  $.get('dreamTeam', function (response) {
+    let dreamTeam = { allPlayers: response };
+    console.log(dreamTeam);
+
+    $('.teamCard').empty();
+
+    var newHTML = template(dreamTeam);
+    $('.teamCard').append(newHTML);
+  });
+});
+
+$('body').on('click', '.playerCard', function () {
+  let firstName = $(this).find('#firstName')[0].innerHTML;
+  console.log(firstName);
+  let lastName = $(this).find('.lastName')[0].innerHTML;
+  let jersey = $(this).find('#jersey')[0].innerHTML;
+  let photo = $(this).find('.clickable').find('.photoCard').attr('src');
+  let position = $(this).find('#position')[0].innerHTML;
+
+  let data = {
+    firstName: firstName,
+    lastName: lastName,
+    jersey: jersey,
+    position: position,
+    photo: photo,
+  };
+  console.log(data);
+  $.post('addPlayer', data, function (response) {
+    response.send(data);
+  });
+});
